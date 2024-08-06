4571768cab8be417cb591d325c4f074e1f8984ba
simrangoel0 <146117601+simrangoel0@users.noreply.github.com>
2024-08-07 00:33:27 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import {getFirestore } from "firebase/firestore";
 import {getAuth } from "firebase/auth";
 import { getVertexAI, getGenerativeModel } from "firebase/vertexai-preview";
@@ -22,7 +23,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-export const auth = getAuth(app);
+const auth = getAuth(app);
 
 export {firestore, model};
 
