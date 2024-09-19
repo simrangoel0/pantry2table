34588f8fdd1c1c89607d23aa75083aea5dfa771c
simrangoel0 <146117601+simrangoel0@users.noreply.github.com>
2024-09-19 01:07:34 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ import { getVertexAI, getGenerativeModel } from "firebase/vertexai-preview";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: FIREBASE_API,
+  apiKey: REACT_APP_FIREBASE_API,
   authDomain: "pantry2table-fa81f.firebaseapp.com",
   projectId: "pantry2table-fa81f",
   storageBucket: "pantry2table-fa81f.appspot.com",
